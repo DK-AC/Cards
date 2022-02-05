cccfdd3458ca8d7f0e3f80c633b5608c25f55bba
DK-AC <denkacaj@gmail.com>
2022-02-05 18:06:16 +0300

    control input

--- src/ui/auth/Register/Register.tsx
+++ src/ui/auth/Register/Register.tsx
@@ -1,24 +1,47 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import styles from './Register.module.css'
 
 export const Register = () => {
 
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
-    const [confPassword, setConfPassword] = useState<string>('')
+    const [confirmPassword, setConfirmPassword] = useState<string>('')
+
+    const onChangeEmailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+    const onChangePasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }
+    const onChangeConfirmPasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setConfirmPassword(e.currentTarget.value)
+    }
+
 
     return (
         <div>
             <h1>Cards</h1>
             <h2>Sign Up</h2>
             <div className={styles.input}>
-                <input type="text" placeholder={'Email*'}/>
+                <input type="text"
+                       placeholder={'Email*'}
+                       value={email}
+                       onChange={onChangeEmailHandler}
+                />
             </div>
             <div className={styles.input}>
-                <input type="password" placeholder={'Password*'}/>
+                <input type="password"
+                       placeholder={'Password*'}
+                       value={password}
+                       onChange={onChangePasswordHandler}
+                />
             </div>
             <div className={styles.input}>
-                <input type="password" placeholder={'Confirm password*'}/>
+                <input type="password"
+                       placeholder={'Confirm password*'}
+                       value={confirmPassword}
+                       onChange={onChangeConfirmPasswordHandler}
+                />
             </div>
             <button>Register</button>
         </div>
