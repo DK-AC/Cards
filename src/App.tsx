212ab7adaa477dcf6ffe0a61e13ce98bd5936367
unclestifler <ambalexey@gmail.com>
2022-02-05 19:18:30 +0100

    Навигация компонета APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Register} from "./ui/auth/Register/Register";
+import {Login} from "./ui/auth/Login/Login";
+import {ForgotPass} from "./ui/auth/ForgotPass/ForgotPass";
+import {Routes, Route} from "react-router-dom"
 
 function App() {
     return (
         <div className="App">
-            <Register/>
+            <Routes>
+                <Route path="/" element={<Register/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/fogotpassword" element={<ForgotPass/>}/>
+            </Routes>
         </div>
     );
 }
