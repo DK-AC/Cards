42211023da437918de65f879ee3a60da9de75a01
DK-AC <denkacaj@gmail.com>
2022-02-25 17:04:28 +0300

    change BaseURL

--- src/dal/authApi.ts
+++ src/dal/authApi.ts
@@ -1,8 +1,8 @@
 import axios, {AxiosResponse} from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://neko-back.herokuapp.com/2.0',
-    baseURL: 'http://localhost:7542/2.0',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
+    // baseURL: 'http://localhost:7542/2.0',
     withCredentials: true,
     // переменное окружение
     // baseURL: process.env.REACT_APP_BASE_URL||'http://localhost:7542/2.0',
--- src/dal/packsApi.ts
+++ src/dal/packsApi.ts
@@ -2,8 +2,8 @@ import axios, {AxiosResponse} from "axios";
 import {PackType} from "../bll/reducers/packReducer";
 
 const instance = axios.create({
-    // baseURL: 'https://neko-back.herokuapp.com/2.0',
-    baseURL: 'http://localhost:7542/2.0',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
+    // baseURL: 'http://localhost:7542/2.0',
     withCredentials: true,
 })
 
